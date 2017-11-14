7af22e1c99787446cfec3cbe5215e993c106b27b
Mark_Zhang <Mark_Zhang@email.lingan.com.cn>
2017-11-14 10:28:14 +0800

    20171114-2

--- src/test/java/org/testJenkins/TestGrid.java
+++ src/test/java/org/testJenkins/TestGrid.java
@@ -23,7 +23,7 @@ public class TestGrid {
 	@BeforeTest
 	public void launchapp(String browser) throws MalformedURLException {
 		//String URL = "http://springmvc-wfproject5.openshift.axies.org/springmvc/";
-		String URL = "https://www.baidu.com";
+		String URL = "http://www.baidu.com";
 		//String URL = "http://la-springmvc.lingandev.svc:8080/springmvc/index.jsp";
 		//String URL = "http://10.131.0.124:8080/springmvc/index.jsp";
 		try {
